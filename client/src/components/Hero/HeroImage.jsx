e2e61ad958aee854d00bccfb9a59f48b28671c5e
Ayush Mehta <mehtaayushkumar217@gmail.com>
2026-09-19 11:10:14 +0530

    git commit -m "image updated"

--- client/src/components/Hero/HeroImage.jsx
+++ client/src/components/Hero/HeroImage.jsx
@@ -1,5 +1,4 @@
 import "./HeroImage.css";
-import heroImg from "./hero.jpg";
 
 import FloatingDonationCard from "./FloatingDonationCard";
 import DonorCard from "./DonorCard";
@@ -9,7 +8,7 @@ const HeroImage = () => {
     <div className="hero-image-container">
 
       <img
-        src={heroImg}
+        src={"https://images.unsplash.com/photo-1638526970908-b18e32b0bc42?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGRvbmF0aW9ufGVufDB8fDB8fHww"}
         alt="HopeHands"
         className="hero-main-image"
       />
